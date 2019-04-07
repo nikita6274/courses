e15930a48edbd1eb00f9dbfd8c9412c93b500825
nikita <ilya@ilya-Lenovo-IdeaPad-Z510>
2019-04-07 16:47:26 +0300

    task1

---
+++ Nikita/Store/Controller/Router.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Nikita\Store\Controller;
+
+class Router implements \Magento\Framework\App\RouterInterface {
+
+    /**
+     * @var \Magento\Framework\App\ActionFactory
+     */
+    protected $actionFactory;
+
+    /**
+     * Response
+     *
+     * @var \Magento\Framework\App\ResponseInterface
+     */
+    protected $_response;
+
+    /**
+     * @param \Magento\Framework\App\ActionFactory $actionFactory
+     * @param \Magento\Framework\App\ResponseInterface $response
+     */
+    public function __construct(
+        \Magento\Framework\App\ActionFactory $actionFactory, \Magento\Framework\App\ResponseInterface $response, \Nikita\Store\Helper\Data $helperData)
+    {
+        $this->actionFactory = $actionFactory;
+        $this->_response = $response;
+        $this->helperData = $helperData;
+    }
+
+    /**
+     * Validate and Match
+     *
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @return bool
+     */
+    public function match(\Magento\Framework\App\RequestInterface $request)
+    {
+        $identifier = trim($request->getPathInfo(), '/');
+        if (strpos($identifier, 'exampletocms') !== false) {
+            $request->setModuleName('cms')->setControllerName('page')->setActionName('view')->setParam('page_id', 4); // specify the page id
+        } else if (strpos($identifier, $this->helperData->getFrontUrl()) !== false) {
+            $request->setModuleName('mystore')->setControllerName('test')->setActionName('test');
+        } else {
+            return;
+        }
+
+        return $this->actionFactory->create(
+            'Magento\Framework\App\Action\Forward', ['request' => $request]
+        );
+    }
+
+}
+
